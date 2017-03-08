fad3f59ed48902a19161b211bf533bd996b9e6f1
Linus <Jan.L.Steuler@mnd.thm.de>
2017-03-08 13:16:04 +0100

    l

---
+++ DiaryAPI/src/edu/napier/geo/diary/CalendarEntry.java
@@ -0,0 +1,255 @@
+package edu.napier.geo.diary;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import edu.napier.geo.common.Location;
+
+public class CalendarEntry implements Serializable {
+
+	private static final long serialVersionUID = -4638363557017293395L;
+	private LocalDateTime start;
+	private LocalDateTime end;
+	private String description = null;
+	private String summary = null;
+	private Location location = null;
+	private String uid = null;
+	private ArrayList<Ressource> allocatedRessources = new ArrayList<Ressource>();
+
+	//only required parameters
+	/** Constructor creating a new CalendarEntry with only the required parameters
+	 * @param yearStart year of the start instant in time
+	 * @param monthStart month of the start instant in time
+	 * @param dayStart day of the start instant in time
+	 * @param hourStart hour of the start instant in time
+	 * @param minuteStart minute of the start instant in time
+	 * @param yearEnd year of the end instant in time
+	 * @param monthEnd month of the end instant in time
+	 * @param dayEnd day of the end instant in time
+	 * @param hourEnd hour of the end instant in time
+	 * @param minuteEnd minute of the end instant in time
+	 * @throws StartEndException if the event end is earlier than the event start
+	 */
+	public CalendarEntry (int yearStart, int monthStart, int dayStart, int hourStart, int minuteStart,
+			int yearEnd, int monthEnd, int dayEnd, int hourEnd, int minuteEnd) throws StartEndException{
+		start = LocalDateTime.of(yearStart, monthStart, dayStart, hourStart, minuteStart);
+		end = LocalDateTime.of(yearEnd, monthEnd, dayEnd, hourEnd, minuteEnd);
+		if (start.isAfter(end))
+			throw new StartEndException();
+		this.uid = UUID.randomUUID().toString();
+		uid = uid+"napier";
+	}
+
+	//all parameters
+	/** Constructor creating a new CalendarEntry with all parameters
+	 * @param yearStart year of the start instant in time
+	 * @param monthStart month of the start instant in time
+	 * @param dayStart day of the start instant in time
+	 * @param hourStart hour of the start instant in time
+	 * @param minuteStart minute of the start instant in time
+	 * @param yearEnd year of the end instant in time
+	 * @param monthEnd month of the end instant in time
+	 * @param dayEnd day of the end instant in time
+	 * @param hourEnd hour of the end instant in time
+	 * @param minuteEnd minute of the end instant in time
+	 * @param description description/title of the calendar entry
+	 * @param summary summary of the calendar entry
+	 * @param location location associated with the event of the calendar entry
+	 * @param ressources ressources associated with the event of the calendar entry
+	 * @throws StartEndException if the event end is earlier than the event start
+	 */
+	public CalendarEntry (int yearStart, int monthStart, int dayStart, int hourStart, int minuteStart,
+			int yearEnd, int monthEnd, int dayEnd, int hourEnd, int minuteEnd,
+			String description, String summary, Location location, ArrayList<Ressource> ressources) throws StartEndException{
+
+		start = LocalDateTime.of(yearStart, monthStart, dayStart, hourStart, minuteStart);
+		end = LocalDateTime.of(yearEnd, monthEnd, dayEnd, hourEnd, minuteEnd);
+		if (start.isAfter(end))
+			throw new StartEndException();
+
+		this.description = description;
+		this.summary = summary;
+		this.location = location;
+		this.uid = UUID.randomUUID().toString();
+		uid = uid+"napier";
+		allocatedRessources = ressources;
+	}
+
+	//Constructor using LocalDateTimes and all parameters
+	/** Constructor creating a new CalendarEntry with all parameters
+	 * @param start start instant of time
+	 * @param end end instant of time
+	 * @param description description/title of the calendar entry
+	 * @param summary summary of the calendar entry
+	 * @param location location associated with the event of the calendar entry
+	 * @param ressources ressources associated with the event of the calendar entry
+	 * @throws StartEndException if the event end is earlier than the event start
+	 */
+	public CalendarEntry (LocalDateTime start, LocalDateTime end, String description, String summary, Location location, ArrayList<Ressource> ressources) throws StartEndException{
+		this(start.getYear(),start.getMonthValue(), start.getDayOfMonth(),start.getHour(), start.getMinute(),
+				end.getYear(),end.getMonthValue(), end.getDayOfMonth(),end.getHour(), end.getMinute(),
+				description, summary, location, ressources);
+	}
+
+	//Constructor using LocalDateTimes and only required parameters
+	/** Constructor creating a new CalendarEntry with only the required parameters
+	 * @param start start instant of time
+	 * @param end end instant of time
+	 * @throws StartEndException if the event end is earlier than the event start
+	 */
+	public CalendarEntry (LocalDateTime start, LocalDateTime end) throws StartEndException{
+		this(start.getYear(),start.getMonthValue(), start.getDayOfMonth(),start.getHour(), start.getMinute(),
+				end.getYear(),end.getMonthValue(), end.getDayOfMonth(),end.getHour(), end.getMinute());
+	}
+
+	public String toString(){
+		String startMinuteString, endMinuteString, startHourString, endHourString;
+		startMinuteString = ""+start.getMinute();
+		endMinuteString = ""+end.getMinute();
+		startHourString =""+start.getHour();
+		endHourString=""+end.getHour();
+
+		if (start.getMinute()==0)
+			startMinuteString="00";
+		if (start.getHour()==0)
+			startHourString="00";
+		if (end.getMinute()==0)
+			endMinuteString="00";
+		if (end.getHour()==0)
+			endHourString="00";
+
+		String locationstring=null;
+		String ressourcestring=null;
+		if (location != null)
+			locationstring = location.getDescription();
+		if (locationstring==null)
+			locationstring = "no location set";
+		if (allocatedRessources != null)
+			ressourcestring = allocatedRessources.toString();
+		if (ressourcestring==null)
+			ressourcestring = "no ressources allocated";
+		if (description==null)
+			description = "no description";
+		if (summary==null)
+			summary = "no summary";
+		
+		return "Event start:        "+start.getDayOfMonth()+"."+start.getMonth()+" "+start.getYear()+" "+startHourString+":"+startMinuteString+"\n"
+		+"Event end:          "+end.getDayOfMonth()+"."+end.getMonth()+" "+end.getYear()+" "+endHourString+":"+endMinuteString+"\n"
+		+"Event description:  "+description+"\n"
+		+"Event summary:      "+summary+"\n"
+		+"UniqueID:           "+uid+"\n"
+		+"Location:           "+locationstring+"\n"
+		+"Ressources:         "+ressourcestring+"\n"
+		;
+
+	}
+
+	/** getter for the scheduled start instant of time
+	 * @return start instant of time
+	 */
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	/** setter for the scheduled start instant of time
+	 * @param start scheduled start instant of time
+	 */
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	/** getter for the scheduled end instant of time
+	 * @return end instant of time
+	 */
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	/** setter for the scheduled end instant of time
+	 * @param end scheduled end instant of time
+	 */
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	/** getter for the event description
+	 * @return event description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/** setter for the event description
+	 * @param description event description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/** getter for the event summary
+	 * @return event summary
+	 */
+	public String getSummary() {
+		return summary;
+	}
+
+	/** setter for the event summary
+	 * @param summary event summary
+	 */
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	/** getter for the Location
+	 * @return Location of the event
+	 */
+	public Location getLocation() {
+		return location;
+	}
+
+	/** setter for the Location
+	 * @param location Location of the event
+	 */
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	/** getter for the event's UID
+	 * @return the event's UID
+	 */
+	public String getUid() {
+		return uid;
+	}
+
+	/** setter for the event's UID - should only be used when external events are imported
+	 * @param uid the UID to set
+	 */
+	protected void setUid(String uid){
+		this.uid = uid;
+	}
+
+	/** getter for the allocated Ressources
+	 * @return ArrayList of allocated Ressources
+	 */
+	public ArrayList<Ressource> getAllocatedRessources() {
+		return allocatedRessources;
+	}
+
+	//should only be accessed from the Facade class to ensure correct listing of Events and Ressources!
+	/** adds Ressources to the CalendarEntry
+	 * @param res ArrayList of Ressources to add
+	 */
+	protected void addRessources(ArrayList<Ressource> res){
+		allocatedRessources.addAll(res);
+	}
+	
+	/** removes Ressources from the CalendarEntry
+	 * @param res ArrayList of Ressources to remove
+	 */
+	protected void removeRessources(ArrayList<Ressource> res){
+		allocatedRessources.removeAll(res);
+	}
+
+}
